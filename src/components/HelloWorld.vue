8e65e0c749e3b9c8e4a734c9c553b64611b4cf4e
Diego Zucchet <diezucchet@gmail.com>
2025-02-05 00:00:12 -0500

    chore(eslint): configure eslint and prittier

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
 defineProps({
   msg: String,
-})
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -30,8 +30,9 @@ const count = ref(0)
     <a
       href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
       target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
+    >
+      Vue Docs Scaling up Guide.
+    </a>
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>

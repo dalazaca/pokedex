a2b4d9113b08b54f9f3d274458e6ed38c496c4eb
Diego Zucchet <diezucchet@gmail.com>
2025-02-06 01:48:56 -0500

    feat(ui): add PokeItem and PokeSearch components with icons

---
+++ src/components/ui/PokeItem.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="poke-item d-flex justify-content-between" @click="seeDetails">
+    <p>{{ name }}</p>
+    <div class="image-wrapper" @click.stop="toogleFavorite">
+      <img
+        v-if="favorite"
+        src="@/assets/icons/favorite-active.svg"
+        alt="favorite active"
+      />
+      <img
+        v-else
+        src="@/assets/icons/favorite-inactive.svg"
+        alt="favorite inactive"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  favorite: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['toogleFavorite', 'seeDetails']);
+
+function seeDetails() {
+  emit('seeDetails', props.name);
+}
+
+function toogleFavorite() {
+  emit('toogleFavorite', props.name);
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/custom.scss';
+
+.poke-item {
+  width: 100%;
+  position: relative;
+  padding: 8px 10px;
+  border: 1px solid $white;
+  background-color: $white;
+  border-radius: 5px;
+  height: 60px;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover {
+    border: 1px solid $gray-300;
+  }
+
+  p {
+    margin-bottom: 0;
+    font-weight: $font-weight-medium;
+    font-size: $h3-font-size;
+    color: $gray-900;
+    padding-left: 10px;
+    line-height: 40px;
+  }
+
+  .image-wrapper {
+    position: relative;
+    width: 44px;
+    height: 44px;
+    border-radius: 100px;
+    background-color: $gray-100;
+
+    &:hover {
+      background-color: $gray-300;
+    }
+
+    img {
+      position: absolute;
+      margin: auto;
+      right: 0;
+      left: 0;
+      top: 0;
+      bottom: 0;
+    }
+  }
+}
+</style>
---
+++ src/components/ui/PokeSearch.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="poke-search input-group mb-3">
+    <span class="input-group-text">
+      <img src="@/assets/icons/search.svg" alt="search pokemon" />
+    </span>
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Search"
+      aria-label="Search"
+      :modelValue="modelValue"
+      @update:modelValue="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps(['modelValue']);
+defineEmits(['update:modelValue']);
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/custom.scss';
+
+.poke-search {
+  box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.04);
+  border-radius: 5px;
+  background-color: $white;
+
+  &:hover {
+    box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.08);
+  }
+
+  .input-group-text {
+    padding: 15px 0 16px 16px;
+    border: none;
+    background-color: $white;
+  }
+
+  .form-control {
+    border: none;
+    background-color: $white;
+    padding: 14px 10px;
+    font-size: $font-size-sm;
+    color: $gray-900;
+
+    &:focus {
+      border: none;
+      box-shadow: none;
+    }
+
+    &:placeholder {
+      color: $gray-500;
+    }
+  }
+}
+</style>
